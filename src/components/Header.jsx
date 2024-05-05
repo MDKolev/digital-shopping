0319c96a2eb3310d83fd10d56a80dd6425884951
MDKolev <113345588+MDKolev@users.noreply.github.com>
2024-05-05 18:58:33 +0300

    adding Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,11 @@
-import { Link } from "react-router-dom";
-import logo from "../assets/logo.svg";
+import { Link, NavLink } from "react-router-dom";
 import Navbar from "./Navbar";
 import { useState } from "react";
 import { MdMenu, MdClose } from "react-icons/md";
+import { FaOpencart } from "react-icons/fa";
+import logo from "../assets/logo.svg";
+import logout from "../assets/logout.svg";
+import user from "../assets/user.svg";
 
 const Header = () => {
   const [menuOpened, setMenuOpened] = useState(false);
@@ -12,7 +15,7 @@ const Header = () => {
   };
 
   return (
-    <header className="fixed top-0 left-0 m-auto max_padd_container w-full bg-white rin-1 ring-slate-900/5 z-10">
+    <header className="fixed top-0 left-0 m-auto max_padd_container w-full bg-white ring-1 ring-slate-900/5 z-10">
       <div className="px-4 flexBetween py-3 max-xs:px-2 ">
         {/* logo */}
         <div>
@@ -35,12 +38,43 @@ const Header = () => {
         />
 
         {/* buttons */}
-        <div >
+        <div className="flexBetween sm:gap-x-2 bold-16">
           {!menuOpened ? (
-            <MdMenu className="md:hidden cursor-pointer hover:text-secondary mr-2 p-1 ring-1 ring-slate-900/39 h-8 w-8 rounded-full" onClick={toggleManu}/>
+            <MdMenu
+              className="md:hidden cursor-pointer hover:text-secondary mr-2 p-1 ring-1 ring-slate-900/39 h-8 w-8 rounded-full"
+              onClick={toggleManu}
+            />
           ) : (
-            <MdClose className="md:hidden cursor-pointer hover:text-secondary mr-2 p-1 ring-1 ring-slate-900/39 h-8 w-8 rounded-full" onClick={toggleManu} />
+            <MdClose
+              className="md:hidden cursor-pointer hover:text-secondary mr-2 p-1 ring-1 ring-slate-900/39 h-8 w-8 rounded-full"
+              onClick={toggleManu}
+            />
           )}
+
+          <div className="flexBetween sm:gap-x-6">
+            <NavLink to={"cart-page"} className={"flex"}>
+              <FaOpencart className="p-1 h-8 w-8 ring-slate-900/30 ring-1 rounded-full" />
+              <span className="relative flexCenter w-5 h-5 rounded-full bg-secondary text-white medium-14 -top-2">
+                0
+              </span>
+            </NavLink>
+
+            <NavLink
+              to={"logout"}
+              className={"btn_secondary_rounded flexCenter gap-x-2 medium-16 hidden"}
+            >
+              <img src={logout} alt="logout" height={19} width={19} />
+              Logout
+            </NavLink>
+
+            <NavLink
+              to={"login"}
+              className={"btn_secondary_rounded flexCenter gap-x-2 medium-16"}
+            >
+              <img src={user} alt="userIcon" height={19} width={19} />
+              Login
+            </NavLink>
+          </div>
         </div>
       </div>
     </header>
