1d08e77666ad8f0fb5b99449d282c4fd92bbfa7a
MDKolev <113345588+MDKolev@users.noreply.github.com>
2024-05-05 13:07:34 +0300

    creating header and navbar components, creating componets for pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,28 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Category from "./pages/Category";
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+
 export default function App() {
-  return <main className="bg-primary text-tertiary"></main>;
+  return (
+    <main className="bg-primary text-tertiary">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/mens" element={<Category/>}></Route>
+          <Route path="/womens" element={<Category/>}></Route>
+          <Route path="/kids" element={<Category/>}></Route>
+          <Route path="/product" element={<Product/>}>
+            <Route path=":productId" element={<Product/>}></Route>
+          </Route>
+          <Route path="/cart" element={<Cart/>}></Route>
+          <Route path="/login" element={<Login/>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </main>
+  );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import { MdCategory, MdContacts, MdHomeFilled, MdShop2 } from "react-icons/md";
+import { NavLink } from "react-router-dom";
+
+const Navbar = ({ containerStyles }) => {
+  return (
+    <nav className={`${containerStyles}`}>
+        <NavLink to={"/"}><div className="flexCenter gap-x-1 "><MdHomeFilled />Home</div></NavLink>
+        <NavLink to={"/mens"}><div className="flexCenter gap-x-1 "><MdCategory /> Men's</div></NavLink>
+        <NavLink to={"/womens"}><div className="flexCenter gap-x-1 "><MdShop2 />Women's</div></NavLink>
+        <NavLink to={"/kids"}><div className="flexCenter gap-x-1 "><MdContacts />Kid's</div></NavLink>
+    </nav>
+  );
+};
+
+export default Navbar;
