df8fcaf7c2e7f18f5965d20424ced8ff5b24a13f
MDKolev <113345588+MDKolev@users.noreply.github.com>
2024-05-06 11:34:48 +0300

    adding Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
 import FOOTER_LINKS from "../assets/footer_links";
 import FOOTER_CONTACT_INFO from "../assets/footer_contact";
+import SOCIALS from "../assets/socials";
 
 const Footer = () => {
   return (
-    <footer>
-      <div>
-        <div>
+    <footer className="flexCenter bg-24 pt-20">
+      <div className="max_padd_container flex w-ful flex-col gap-14">
+        <div className="flex flex-col items-start justify-center gap-[10%] md:flex-row">
           <Link to="/" className="mb-10 bold-20 ">
             Shoppee
           </Link>
@@ -22,16 +23,37 @@ const Footer = () => {
                 </ul>
               </FooterColumn>
             ))}
-            <div>
+            <div className="flex flex-col gap-5">
               <FooterColumn title={FOOTER_CONTACT_INFO.title}>
                 {FOOTER_CONTACT_INFO.links.map((link) => (
-                  <Link to="/" key={link.label}>
+                  <Link
+                    to="/"
+                    key={link.label}
+                    className="flex gap-4 md:flex-col lg:flex-row"
+                  >
                     <p>{link.label}</p>
+                    <p className="medium-14">{link.value}</p>
                   </Link>
                 ))}
               </FooterColumn>
             </div>
+            <div className="flex">
+              <FooterColumn>
+                <ul className="flex gap-4">
+                  {SOCIALS.links.map((link) => (
+                    <Link to="/" key={link}>
+                      <img src={link} alt="socialIcon" width={22} height={22} />
+                    </Link>
+                  ))}
+                </ul>
+              </FooterColumn>
+            </div>
           </div>
+        </div>
+        <div className="border bg-gray-20">
+          <p className="text-center regular-14 text-gray-30">
+            2024 Shoppee | All rights reserved.{" "}
+          </p>
         </div>
       </div>
     </footer>
@@ -40,8 +62,8 @@ const Footer = () => {
 
 const FooterColumn = ({ title, children }) => {
   return (
-    <div>
-      <h4>{title}</h4>
+    <div className="flex flex-col gap-5 ">
+      <h4 className="bold-18 whitespace-nowrap">{title}</h4>
       {children}
     </div>
   );
