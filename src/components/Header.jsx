0a5ce018ec85ee10322d3ab5efe181512d991387
MDKolev <113345588+MDKolev@users.noreply.github.com>
2024-05-07 18:14:36 +0300

    creating CartItems component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import { Link, NavLink } from "react-router-dom";
 import Navbar from "./Navbar";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { MdMenu, MdClose } from "react-icons/md";
 import { FaOpencart } from "react-icons/fa";
 import logo from "../assets/logo.svg";
 import logout from "../assets/logout.svg";
 import user from "../assets/user.svg";
+import { ShopContext } from "../Context/ShopContext";
 
 const Header = () => {
   const [menuOpened, setMenuOpened] = useState(false);
@@ -13,6 +14,8 @@ const Header = () => {
   const toggleManu = () => {
     setMenuOpened(!menuOpened);
   };
+
+  const { getTotalCartItems } = useContext(ShopContext);
 
   return (
     <header className="fixed top-0 left-0 m-auto w-full bg-white ring-1 ring-slate-900/5 z-10">
@@ -55,7 +58,7 @@ const Header = () => {
             <NavLink to={"cart-page"} className={"flex"}>
               <FaOpencart className="p-1 h-8 w-8 ring-slate-900/30 ring-1 rounded-full" />
               <span className="relative flexCenter w-5 h-5 rounded-full bg-secondary text-white medium-14 -top-2">
-                0
+                {getTotalCartItems()}
               </span>
             </NavLink>
 
