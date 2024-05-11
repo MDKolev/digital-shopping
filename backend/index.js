7a9470cdf4abcd6061f8fbc368a2134176931109
MDKolev <113345588+MDKolev@users.noreply.github.com>
2024-05-11 13:57:22 +0300

    implementing showing products from database

--- backend/index.js
+++ backend/index.js
@@ -28,11 +28,11 @@ const storage = multer.diskStorage({
     }
 });
 
-const upload = multer({storage:storage})
+const upload = multer({ storage: storage })
 
 // creating upload endpoint for images
-app.use("/images", express.static("upload/images"))
-app.post("/upload", upload.single("product"), (req,res) => {
+app.use('/images', express.static('upload/images'))
+app.post("/upload", upload.single('product'), (req, res) => {
     res.json({
         success: 1,
         image_url: `https://localhost:${port}/images/${req.file.filename}`
@@ -75,13 +75,13 @@ const Product = mongoose.model("Product", {
     },
 })
 
-app.post("/addproduct", async(req, res) => {
+app.post("/addproduct", async (req, res) => {
     let products = await Product.find({});
     let id;
-    if(products.length > 0) {
+    if (products.length > 0) {
         let last_product_array = products.slice(-1);
-        let last_product =  last_product_array[0];
-        id = last_product.id +1;
+        let last_product = last_product_array[0];
+        id = last_product.id + 1;
     } else {
         id = 1;
     }
@@ -103,8 +103,8 @@ app.post("/addproduct", async(req, res) => {
 })
 
 // creating api for add product
-app.post("/removeproduct", async(req, res) => {
-    await Product.findOneAndDelete({id:req.body.id});
+app.post("/removeproduct", async (req, res) => {
+    await Product.findOneAndDelete({ id: req.body.id });
     console.log("Removed");
     res.json({
         success: true,
@@ -113,13 +113,89 @@ app.post("/removeproduct", async(req, res) => {
 })
 
 // creating api for get all products
-app.get("/allproducts", async(req, res) => {
-    let products = await Product.find({ });
-        console.log("All products Fetched");
-        res.send(products);
+app.get("/allproducts", async (req, res) => {
+    let products = await Product.find({});
+    console.log("All products Fetched");
+    res.send(products);
 })
 
+// Schema user model
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+    },
+    email: {
+        type: String,
+        unique: true,
+    },
+    password: {
+        type: String,
+    },
+    carData: {
+        type: Object,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+})
 
+// Creating endpoint for registering the user
+app.post('/signup', async(req,res) => {
+    let check = await User.findOne({email: req.body.email});
+    if(check) {
+        return res.status(400).json({success: false, errors: "Existing user found with same email address"});
+    }
+
+    let cart = {};
+    for (let i=0; i<300; i++) {
+        cart[i] = 0;
+    }
+    const user = new User({
+        name: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+        cartData: cart,
+    })
+    await user.save();
+
+    const data = {
+        user: {
+            id: user.id
+        }
+    }
+
+    const token = jwt.sign(data, 'secret_ecom');
+    res.json({success: true, token})
+})
+
+// creating endpoint for use login
+app.post('/login', async (req, res) => {
+    let user = await User.findOne({email:req.body.email});
+    if(user) {
+        const passwordMatch = req.body.password === user.password;
+        if(passwordMatch) {
+            const data = {
+                user: {
+                    id: user.id
+                }
+            }
+            const token = jwt.sign(data, 'secret_ecom')
+            res.json({success: true, token})
+        } else {
+            res.json({success: false, errors:"Wrong Password"})
+        }
+    } else {
+        res.json({success: false, errors:"Wrong Email address"})
+    }
+})
+
+// creating endpoint for latest products
+app.get('/newcollection', async (req, res) => {
+    let products = await Product.find({});
+    let newCollection = products.slice(1).slice(-8);
+    res.send(newCollection)
+})
 
 app.listen(port, (error) => {
     if (!error) {
